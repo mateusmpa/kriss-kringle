9ed108556849a14fe7e1a9e7e868f9525c914c80
Mateus Alves <mateusmpa@gmail.com>
2018-02-12 14:51:36 -0200

    Create coffeescript to include Members in the Campaign

--- app/jobs/campaign_raffle_job.rb
+++ app/jobs/campaign_raffle_job.rb
@@ -1,12 +1,12 @@
 class CampaignRaffleJob < ApplicationJob
-  queue_as :email
+  queue_as :emails
 
   def perform(campaign)
     result = RaffleService.new(campaign).call
 
     campaign.members.each { |m| m.set_pixel }
     result.each do |r|
-      CampaignMailer.raffle(campaign, r.first, r.last).delivery_now
+      CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
     end
     campaign.update(status: :finished)
 
